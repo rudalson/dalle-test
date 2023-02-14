aa302a94033cc3469cc7619911bebc82001f8f7f
rudalson <rudalson@gmail.com>
2023-02-15 08:34:05 +0900

    ➕ add: all posts of card style showing and download

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -18,6 +18,32 @@ const Home = () => {
 
   const [searchText, setSearchText] = useState('');
 
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
